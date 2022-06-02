9f965cef13a9100957e2c1271d39b7cb76e37014
mycomputer <a1557944@outlook.com>
2022-06-02 11:12:37 +0800

    修改了一些逻辑

---
+++ src/main/java/com/example/demo/CommonResult.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class CommonResult<T>{
+    public Integer code;
+    public String message;
+    public T data;
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public CommonResult(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
